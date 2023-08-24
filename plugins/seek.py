6780abd8f6f20497c1e320a6a2977cd0a1d74101
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-24 18:42:27 +0300

    Update seek.py

--- plugins/seek.py
+++ plugins/seek.py
@@ -18,7 +18,7 @@ SEEK_COMMAND = get_command("SEEK_COMMAND")
 
 
 @app.on_message(
-    filters.command(SEEK_COMMAND)
+    filters.command(["SEEK_COMMAND"],prefixes= ["/", "!","","#"]))
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS

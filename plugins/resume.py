9d4f2930b83b09eba9b19f093cd4f8b0b3878a10
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-24 18:41:40 +0300

    Update resume.py

--- plugins/resume.py
+++ plugins/resume.py
@@ -18,7 +18,7 @@ RESUME_COMMAND = get_command("RESUME_COMMAND")
 
 
 @app.on_message(
-    filters.command(RESUME_COMMAND)
+    filters.command(["RESUME_COMMAND"],prefixes= ["/", "!","","#"]))
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
